91c1166d03893380f078f91fe11a23b2d97b4ee0
João Pegoraro <hahmraro@tuta.io>
2022-11-17 18:33:32 -0300

    Criei classe da aplicação e atualizei manifesto

---
+++ app/src/main/java/xyz/joaophp/carroswswork/CarrosApplication.kt
@@ -0,0 +1,32 @@
+package xyz.joaophp.carroswswork
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.core.logger.Level
+import xyz.joaophp.carroswswork.di.networkModule
+
+class CarrosApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            allowOverride(true)
+            androidLogger(if (BuildConfig.DEBUG) Level.DEBUG else Level.NONE)
+            androidContext(this@CarrosApplication)
+            modules(modules)
+        }
+    }
+
+    override fun onTerminate() {
+        stopKoin()
+        super.onTerminate()
+    }
+
+    companion object {
+        private val modules = listOf(networkModule)
+    }
+}
