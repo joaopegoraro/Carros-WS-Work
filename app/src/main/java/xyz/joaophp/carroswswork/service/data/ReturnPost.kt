f10170024d41ea99f64b495fca1a6e52ad388a63
João Pegoraro <hahmraro@tuta.io>
2022-11-17 18:30:20 -0300

    Implmentei endpoints

---
+++ app/src/main/java/xyz/joaophp/carroswswork/service/data/ReturnPost.kt
@@ -0,0 +1,7 @@
+package xyz.joaophp.carroswswork.service.data
+
+import com.google.gson.annotations.SerializedName
+
+data class ReturnPost(
+    @SerializedName("status") val status: String? = null
+)
--- app/src/main/java/xyz/joaophp/carroswswork/service/remote/ApiService.kt
+++ app/src/main/java/xyz/joaophp/carroswswork/service/remote/ApiService.kt
@@ -1,5 +1,27 @@
 package xyz.joaophp.carroswswork.service.remote
 
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import xyz.joaophp.carroswswork.service.data.Carro
+import xyz.joaophp.carroswswork.service.data.Lead
+import xyz.joaophp.carroswswork.service.data.ReturnPost
+
 interface ApiService {
 
+    /**
+     *  GET
+     */
+
+    @GET("cars.json")
+    suspend fun getCarros(): Response<List<Carro>>
+
+
+    /**
+     *  POST
+     */
+
+    @POST("cars/leads")
+    suspend fun postLeads(@Body leads: List<Lead>): Response<ReturnPost>
 }
